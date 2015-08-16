8c227ce1ed8d295a111037a904d3d7bae8c8ac4e
Jan Dvořák <mordae@anilinux.org>
2015-08-16 11:40:08 +0200

    Add missing dependencies
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "typed-racket-lib" "mordae"))
-(define build-deps '("racket-doc" "typed-racket-lib"))
+(define deps '("base" "scribble-lib" "typed-racket-lib" "parser-tools-lib"
+               "mordae"))
+(define build-deps '("racket-doc" "typed-racket-doc" "typed-racket-lib"))
 
 ; vim:set ts=2 sw=2 et:
